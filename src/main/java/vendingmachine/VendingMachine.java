c7f7215d03d19be5a00ef95e3c9a758fe980f543
booyaChael <chaeeul2022@gmail.com>
2023-12-06 20:34:39 +0900

    feat(VendingMachine): 특정 조건이 충족될 때까지 상품 구매 반복

--- src/main/java/vendingmachine/VendingMachine.java
+++ src/main/java/vendingmachine/VendingMachine.java
@@ -20,12 +20,22 @@ import vendingmachine.View.OutputView;
 
 		public void takeOrder(){
 			this.userMoney = Integer.parseInt(InputView.getUserMoney());
+			buy();
+
+		}
+
+		private void buy(){
 			while(true){
 				try{
 					OutputView.printUserMoneyLeft(userMoney);
 					String productName = InputView.getProductToBuy();
 					Product product = findProductByName(productName);
 					buyProduct(product);
+
+					if(findLowestPriceOfCurrentProducts() > userMoney || isNoProductLeft()){
+						break;
+					}
+
 				} catch(IllegalArgumentException e){
 					System.out.println(e.getMessage());
 				}
@@ -43,6 +53,30 @@ import vendingmachine.View.OutputView;
 			return null;
 		}
 
+		//현재 수량이 남아있는 Products에 대해서 가장 낮은 가격을 반환한다
+		private int findLowestPriceOfCurrentProducts() {
+			int lowestPrice = Integer.MAX_VALUE;
+			for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+				Product product = entry.getKey();
+				int currentPrice = product.getPrice();
+				int quantity = entry.getValue();
+
+				if (quantity >= 1 && currentPrice < lowestPrice) {
+					lowestPrice = currentPrice;
+				}
+			}
+			return lowestPrice;
+		}
+
+		private boolean isNoProductLeft(){
+			for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+				if (entry.getValue() != 0) {
+					return false;
+				}
+			}
+			return true;
+		}
+
 		private void buyProduct(Product product){
 			if (!products.containsKey(product)) {
 				throw new IllegalArgumentException("[ERROR] 자판기에 존재하지 않는 상품입니다.");
@@ -51,8 +85,6 @@ import vendingmachine.View.OutputView;
 			if(currentQuantity < 1) {
 				throw new IllegalArgumentException("[ERROR] 재고가 부족합니다.");
 			}
-			System.out.println("I'm here");
-
 			products.put(product, currentQuantity - 1);
 			userMoney -= product.getPrice();
 		}
